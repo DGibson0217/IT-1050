bd39fcccb6e6ba22ebed96c2b7f9b50cfc3ed43e
DGibson0217 <deshawngibson36@gmail.com>
2018-10-18 11:30:58 -0400

    Final Project
    Final changes to the project

---
+++ IT 1050 Project 2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050_Project_2
+{
+    class Person
+    {
+        // Public member variables
+        // Public member variables
+
+
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+
+        public static double SumofAllAges;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            Console.WriteLine("My name is " + this.GetFullName() + ".");
+            Console.WriteLine("My age is " + this.Age + ".");
+        }
+
+
+
+
+    }
+}
+
+
---
+++ IT 1050 Project 2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050_Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Person 1
+            Person p1 = new Person();
+            Console.WriteLine("What is your First Name?");
+            p1.FirstName = Console.ReadLine();
+            Console.WriteLine("What is your Last Name?");
+            p1.LastName = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            p1.Age = int.Parse(Console.ReadLine());
+            p1.Spouse = new Person();
+
+            Person.SumofAllAges = Person.SumofAllAges + p1.Age;
+
+            p1.PrintNameAndAge();
+
+            Console.WriteLine("What is your Spouse's Name?");
+            p1.Spouse.FirstName = Console.ReadLine();
+            p1.Spouse.LastName = p1.LastName;
+            Console.WriteLine("What is your Spouse's Age?");
+            p1.Spouse.Age = int.Parse(Console.ReadLine());
+            p1.Spouse.Spouse = p1;
+
+            p1.Spouse.PrintNameAndAge();
+
+            Person.SumofAllAges = Person.SumofAllAges + p1.Spouse.Age;
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+
+            Person p2 = new Person();
+            Console.WriteLine("What is your First Name?");
+            p2.FirstName = Console.ReadLine();
+            Console.WriteLine("What is your Last Name?");
+            p2.LastName = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            p2.Age = int.Parse(Console.ReadLine());
+            p2.Spouse = new Person();
+
+            Person.SumofAllAges = Person.SumofAllAges + p2.Age; 
+
+            p2.PrintNameAndAge();
+
+            Console.WriteLine("What is your Spouse's Name?");
+            p2.Spouse.FirstName = Console.ReadLine();
+            p2.Spouse.LastName = p2.LastName;
+            Console.WriteLine("What is your Spouse's Age?");
+            p2.Spouse.Age = int.Parse(Console.ReadLine());
+            p2.Spouse.Spouse = p2;
+
+            p2.Spouse.PrintNameAndAge();
+
+            Person.SumofAllAges = Person.SumofAllAges + p2.Spouse.Age;
+
+            Console.WriteLine(" The sum of our ages is ");
+            Console.WriteLine(Person.SumofAllAges);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+            Console.WriteLine("The average of our ages is ");
+            Console.WriteLine(Person.SumofAllAges / 4);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+
+        }
+    }
+}
