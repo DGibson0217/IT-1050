1dae009e4a018b0e26613c95fa388fa7d9bc2f56
DGibson0217 <deshawngibson36@gmail.com>
2018-11-29 17:39:14 -0500

    Update IT-1050 lab 4.cs

---
+++ IT-1050 lab 4.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_1050_lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 1
+            const double smallSoda = 3.50;
+            const double largeSoda = 5.99;
+            const double hotDog = 3.99;
+            const double popCorn = 4.50;
+            const double candy = 1.99;
+
+            const double matineeChild = 3.99;
+            const double matineeAdult = 5.99;
+            const double matineeSenior = 4.50;
+
+            const double eveningChild = 6.99;
+            const double eveningAdult = 10.99;
+            const double eveningSenior = 8.50;           
+
+            bool isMatinee = false;
+            double totalPrice = 0;
+            int addChild;
+            int addAdult;
+            int addSenior;
+            int addsmallSoda;
+            int addlargeSoda;
+            int addhotDog;
+            int addpopCorn;
+            int addcandy;
+
+            Console.WriteLine("Matinee or Evening?");
+            string inputTime = Console.ReadLine();
+            Console.WriteLine();
+
+            if (inputTime == "Matinee")
+            {
+                Console.WriteLine("Welcome to the Matinee Show.");
+                isMatinee = true;
+            }
+            else if (inputTime == "Evening") 
+            {
+                Console.WriteLine("Welcome to the Evening Show.");
+            }
+            else 
+            {
+                Console.WriteLine("We don't have any showing at that time.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("How many Children?");
+            addChild = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("How many Adults?");
+            addAdult = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("How many seniors?");
+            addSenior = int.Parse(System.Console.ReadLine());
+
+            if (isMatinee == true)
+            {
+                totalPrice += (matineeChild * addChild) + (matineeAdult * addAdult) + (matineeSenior * addSenior); 
+            }
+           else if (isMatinee == false)
+            {
+                totalPrice += (eveningChild * addChild) + (eveningAdult * addAdult) + (eveningSenior * addSenior);
+            }
+            #endregion
+            #region Part 2
+            Console.WriteLine("How many Small Sodas would you like?");
+            addsmallSoda = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("How many large sodas would you like?");
+            addlargeSoda = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("How many Hot Dogs would you like?");
+            addhotDog = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("How many Bags of popcorn would you like?");
+            addpopCorn = int.Parse(System.Console.ReadLine());
+            Console.WriteLine("How many bags of candy would you like?");
+            addcandy = int.Parse(System.Console.ReadLine());
+
+            totalPrice += addlargeSoda * largeSoda;
+            totalPrice += addsmallSoda * smallSoda;
+            totalPrice += addhotDog * hotDog;
+            totalPrice += addpopCorn * popCorn;
+            totalPrice += addcandy * candy;
+
+            Console.WriteLine("Total Price before discounts:" + totalPrice);
+            Console.ReadLine();
+            #endregion
+
+            //Discounts
+ 
+            if ((addcandy % 4) == 0)
+            {
+                totalPrice -= (addcandy / 4) * candy;
+            }
+
+            Console.WriteLine(totalPrice);
+            Console.ReadLine();
+
+        }
+    }
+}
